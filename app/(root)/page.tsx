c7d48c1d57e8f70f4848ec07d3952b8e5b93164f
Siddharth Sen <sensiddharth093@gmail.com>
2025-08-20 18:24:45 +0530

    changed page.tsx in app/root group

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,51 +1,94 @@
-import React from 'react'
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import Image from 'next/image';
-import { dummyInterviews } from '@/constants';
-import InterviewCard from '@/components/InterviewCard';
-const page = () => {
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import InterviewCard from "@/components/InterviewCard";
+
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
+
+async function Home() {
+  const user = await getCurrentUser();
+
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
+
   return (
     <>
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
-          <h2 className='text-primary-100'>Get Interview Ready with AI-powered practice and Feedback</h2>
-          <p>
-            Practice job interviews with AI and get personalized feedback to
-            improve your performance. Sign up now to start your journey towards
-            interview success!
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
           </p>
+
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
-            {/**
-             * Basically, here the styles of button 
-             * are going to be applied to the Link component. 
-             */}
           </Button>
         </div>
-        <Image src='/robot.png' alt='Robot Illustration' width={400} height={4} className="max-sm:hidden" />
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
-          <h2>Your Interviews</h2>
-          <div className="interviews-section">
-              {dummyInterviews.map(interview => (
-                <InterviewCard {...interview} key={interview.id}/>
-                )
-              )}
-          </div>
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
+        </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
-          <h2>Take an Interview</h2>
-          <div className="interviews-section">
-              {dummyInterviews.map(interview => (
-                  <InterviewCard {...interview} key={interview.id}/>
-                ))}
-          </div>
+        <h2>Take Interviews</h2>
+
+        <div className="interviews-section">
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
+        </div>
       </section>
     </>
-  )
+  );
 }
 
-export default page
+export default Home;
